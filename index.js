4778055d633603c1d2586cf336e5724eed074470
Soldy <4786022+Soldy@users.noreply.github.com>
2021-09-22 23:51:18 +0100

    clean up
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,23 +1,72 @@
 
+/*
+ *  @Soldy\binaryrc\2020.12.07\GPL3
+ */
+'use strict'
 
-exports.binary=function(){
+
+/*
+ * @prototype
+ */
+const _binary=function(){
+    /*
+     * @param {array}
+     * @public
+     * @retur {string}
+    */
     this.aTs = function(input){
+        return _aTs(input);
+    }
+    /*
+     * @param {string}
+     * @public
+     * @retur {array}
+    */
+    this.sTa8 = function(input){
+        return _sTa8(input);
+    }
+    /*
+     * @param {string}
+     * @public
+     * @retur {array}
+    */
+    this.sTa = function(input){
+        return _sTa(input);
+    }
+    /*
+     * @param {string}
+     * @private
+     * @retur {array}
+    */
+    const _sTa = (input)=>{
+        let pushed = input.split('');
+        const  strLen=input.length;
+        for (let i=0; i < strLen ; i++) 
+            pushed[i]=input.charCodeAt(i);
+        return pushed;
+    }
+    /*
+     * @param {string}
+     * @private
+     * @retur {array}
+    */
+    const _sTa8 = function(input){
+        let pushed = _sTa(input);
+        let buf = new Uint8Array(pushed.length);
+        buf.set(pushed);
+        return buf;
+
+    }
+    /*
+     * @param {array}
+     * @private
+     * @retur {string}
+    */
+    const _aTs = function(input){
         return String.fromCharCode.apply(
             null,
             input
         );
     }
-    this.sTa8 = function(input){
-        let pushed = this.sTa(input);
-        let buf = new Uint8Array(pushed.length);
-        buf.set(pushed);
-        return buf;
-    }
-    this.sTa = function(input){
-        let pushed = input.split('');
-        for (let i=0, strLen=input.length; i < strLen; i++) 
-            pushed[i]=input.charCodeAt(i);
-        return pushed;
-    }
 }
-
+exports.base = _binary;
